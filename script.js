a0359b36d26b7c98d8bec4f28fd3b19b34a1c04f
StandardCdr <jb.andrew0123@gmail.com>
2024-10-09 15:20:26 +0800

    F

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ const navLinks = document.querySelector(".nav-links");
 menuIcon.onclick = () => {
   navLinks.classList.toggle("active");
 };
+
+/*Scroll Reveal */
+
+ScrollReveal({
+  distance: "25px",
+  duration: 2000,
+  delay: 200,
+});
+
+ScrollReveal().reveal("#profile", { origin: "top" });
+ScrollReveal().reveal("#about", { origin: "bottom" });
+ScrollReveal().reveal("#skills", { origin: "left" });
+ScrollReveal().reveal("#projects", { origin: "right" });
+ScrollReveal().reveal("#contact", { origin: "bottom" });
